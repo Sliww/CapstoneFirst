cb36975b53f879c680753fefe9de54c4cab8c139
Sliww <matte19981@live.it>
2024-12-18 00:33:26 +0100

    Debug nel deploy

--- aledobre-db/main.js
+++ aledobre-db/main.js
@@ -34,17 +34,7 @@ server.use('/', reservationRoute);
 
 server.use(manageErrorMessage);
 
-server.use(session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-        secure: process.env.NODE_ENV === 'production',
-        httpOnly: true,
-        sameSite: 'lax',
-        maxAge: 24 * 60 * 60 * 1000
-    }
-}));
+server.use(session());
 
 initDB();
 
