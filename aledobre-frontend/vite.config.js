71d6a5d0b5a38234d2b38da4094aaeb739c8b836
Sliww <matte19981@live.it>
2024-12-19 17:24:49 +0100

    Deploy backend

--- aledobre-frontend/vite.config.js
+++ aledobre-frontend/vite.config.js
@@ -1,9 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   testEnvironment: 'jsdom',
-  plugins: [react()]
-})
+  plugins: [react()],
+  server: {
+    host: true,
+    port: process.env.PORT || 4012,
+    strictPort: true
+  },
+});
 
